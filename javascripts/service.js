e5bc2fdcd06f9b6eb0624d2e0b77304d84bcfbe2
Stefan Wäfler <stefwae@gmx.net>
2014-06-13 11:45:24 +0200

    add geoloc

---
+++ javascripts/service.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+	var options = {
+  		enableHighAccuracy: true,
+  		timeout: 10000,
+  		maximumAge: 0
+	};
+
+	var success = function(pos) {
+		var crd = pos.coords;
+
+		$('.js-current-position').text(crd.latitude + ' ' + crd.longitude);
+	};
+
+	var error = function {
+		console.warn('ERROR(' + err.code + '): ' + err.message);
+	};
+
+	navigator.geolocation.getCurrentPosition(success, error, options);
+});
