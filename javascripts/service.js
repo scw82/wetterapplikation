f97585ff3db74a5f5cb82e132d57db11e7e73f95
Stefan Wäfler <stefwae@gmx.net>
2014-06-20 09:09:45 +0200

    add address to mobile app

--- javascripts/service.js
+++ javascripts/service.js
@@ -10,6 +10,21 @@ $(document).ready(function() {
 		var crd = pos.coords;
 
 		$('.js-current-position').text(crd.latitude + ' / ' + crd.longitude);
+
+		$.ajax({
+  		url: 'https://maps.googleapis.com/maps/api/geocode/json',
+  		data: {
+  		latlng: crd.latitude + ',' + crd.longitude,
+  		sensor: true
+  		},
+  		success: function(data) {
+  		$('.js-loc-address').text(data.results[0].formatted_address);
+      	console.log(data);
+
+  		}
+
+  	});
+
 	};
 
 	var error = function() {
