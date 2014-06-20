3a4fbad965a67fa7d61c4b5c83e9d3cdc44097b1
Stefan Wäfler <stefwae@gmx.net>
2014-06-20 09:22:51 +0200

    alter nav

--- javascripts/service.js
+++ javascripts/service.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
   		url: 'https://maps.googleapis.com/maps/api/geocode/json',
   		data: {
   		latlng: crd.latitude + ',' + crd.longitude,
-  		language: "fr",
+  		language: 'de',
   		sensor: true
   		},
   		success: function(data) {
