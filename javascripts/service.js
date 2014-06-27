773ce29b255ff59b9fd38a053e829d81348effcd
Stefan Wäfler <stefwae@gmx.net>
2014-06-27 09:07:06 +0200

    change service.js

--- javascripts/service.js
+++ javascripts/service.js
@@ -6,6 +6,19 @@ $(document).ready(function() {
   		maximumAge: 0
 	};
 
+	var weatherIcons = {
+	  'clear-day': 'B',
+	  'clear-night': 'C',
+	  'rain': 'R',
+	  'snow': 'X',
+	  'sleet': 'W',
+	  'wind': 'S',
+	  'fog': 'M',
+	  'cloudy': 'N',
+	  'partly-cloudy-day': 'H',
+	  'partly-cloudy-night': 'I'
+	};
+
 	// Default-Sprache setzen
 	if(localStorage.getItem('language') === null) {
 		localStorage.setItem('language', 'de');
@@ -30,6 +43,20 @@ $(document).ready(function() {
 		//console.log(JSON.parse(localStorage.getItem('pos')));
 
 		$('.js-current-position').text(crd.latitude + ' / ' + crd.longitude);
+
+		$.ajax({
+  			url: 'https://api.forecast.io/forecast/4cbf11a0b6a5166782b8d4cb9d5defef/' + crd.latitude + ',' + crd.longitude,
+  			data: {
+  			units: 'si'
+  		},
+  			dataType: 'jsonp',
+  			success: function(data) {
+	  			$('.js-temp').text(data.currently.apparentTemperature + '°C');
+	  			$('.js-windsp').text(data.currently.windSpeed + 'm/s');
+	      		$('.js-symbol').text(weatherIcons[data.currently.icon]);
+  		}
+
+  	});
 
 		$.ajax({
   		url: 'https://maps.googleapis.com/maps/api/geocode/json',
