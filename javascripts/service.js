49cbfb161e93ba5ea91717a92ccfb4c8752f0cdb
Stefan Wäfler <stefwae@gmx.net>
2014-07-04 09:49:02 +0200

    try

--- javascripts/service.js
+++ javascripts/service.js
@@ -56,7 +56,7 @@ $(document).ready(function() {
 			dataType: 'jsonp',
 			success: function(data) {
 				$('.js-current-weather').text(weatherIcons[data.currently.icon]);
-				$('.js-temp').text(data.currently.temperature);
+				$('.js-temp').parseInt(text(data.currently.temperature)) + '°';
 
 				console.log(data);
 			}
